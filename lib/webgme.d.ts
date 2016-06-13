a0e90969e9962c815465863b951489446828ff47
Fred Eisele <fred.eisele@vanderbilt.edu>
2016-06-13 15:56:59 -0500

    added typings to package.json

---
+++ lib/webgme.d.ts
@@ -0,0 +1,48 @@
+/**
+https://editor.webgme.org/docs/source/index.html
+*/
+
+export declare namespace webgme {
+
+  export interface PluginConfig {
+    serialize(): any;
+  }
+
+  export interface PluginBase {
+    activeNode: any;
+    activeSelection: any[];
+    blobClient: any;
+    core: any;
+    gmeConfig: any;
+    logger: any;
+    META: any;
+    namespace: string;
+    pluginMetadata: any;
+    project: any;
+    result: any;
+    rootNode: any;
+
+    addCommitToResult(status: string): void;
+    baseIsMeta(node: any): boolean;
+    configure(config: PluginConfig): void;
+    createMessage(node: any, message: string, serverity: string): void;
+    getConfigStructure(): any;
+    getCurrentConfig(): PluginConfig;
+    getDefaultConfig(): PluginConfig;
+    getDescription(): string;
+    getMetadata(): any;
+    getMetaType(node: any): any;
+    getName(): string;
+    getVersion(): string;
+    initialize(logger: any, blobClient: any, gmeConfig: any): void;
+    isInvalidActiveNode(pluginId: any): any;
+    isMetaTypeOf(node: any, metaNode: any): boolean;
+    main(callback: any): void;
+    save(message: string): void;
+    sendNotification(message: string, callback?: any): void;
+    setCurrentConfig(newConfig: PluginConfig): void;
+    updateMeta(generatedMeta: any): void;
+    updateSuccess(value: boolean, message: TemplateStringsArray): void;
+  }
+
+}
